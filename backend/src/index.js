f2f2b55ddd6b5025c199e0241e12068e965a0e1b
Victor <victor0122@hotmail.com>
2020-05-01 22:58:50 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const server = http.Server(app);
 
 setupWebsocket(server);
 
-mongoose.connect('mongodb://dev:passdev@cluster0-shard-00-00-a6vdj.mongodb.net:27017,cluster0-shard-00-01-a6vdj.mongodb.net:27017,cluster0-shard-00-02-a6vdj.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect('mongodb://dev:{PASSWORD}@cluster0-shard-00-00-a6vdj.mongodb.net:27017,cluster0-shard-00-01-a6vdj.mongodb.net:27017,cluster0-shard-00-02-a6vdj.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
